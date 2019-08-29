244e128676b916c427bd302dd46eaa97bf174f7f
Cody Spering <43628079+SexyJesus@users.noreply.github.com>
2019-08-29 12:32:16 -0400

    Made Script More Descriptive
    Added more descriptive error message. Added Print out messages to each step of the scripting.

--- MilesTVs.py
+++ MilesTVs.py
@@ -5,6 +5,7 @@ print("----------------------TV Automation Script-----------------------")
 print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 #im a fricken arteest
 print("                         ___________________")
+print("                         |     |/   \|     |")
 print("                         | 18              |")
 print("                         | 17    LOBBY     |")
 print("                         | 16              |")
@@ -31,9 +32,10 @@ print("|___________________________________________________________|")
 print("")
 
 while True:
-    tvSelect = input("Make a TV Selection: ")
+    tvSelect = input(">Make a TV Selection (Ctrl-C to quit): ")
 
-    tvArr = ["172.19.16.10",
+    tvArr = [
+    "172.19.16.10",
     "172.19.16.12",
     "172.19.16.36",
     "172.19.16.28",
@@ -50,32 +52,42 @@ while True:
     "172.19.23.219",
     "172.19.20.152",
     "172.19.16.8",
-    "172.19.23.195"]
+    "172.19.23.195"
+    ]
 
     try:
         inputInt = int(tvSelect)
         tvArrNum = inputInt - 1 #because array do the whole 0 thing
 
         if inputInt < 1 or inputInt > 18:
-            print("Please select a number from 1-18")
+            print("***Please select a number from 1-18***")
         else:
             tv = tvArr[tvArrNum]
             #ADB commands below, may need to be cleaned up, may also not even work, sue me.
-            print("the command runs, good job, nerd. TV IP: " + tv)
+            print("Attempting adb kill-server on " + tv)
             one = subprocess.check_output("adb kill-server").decode('utf-8')
             print(one)
+            print("Attempting adb start-server on " + tv)
             two = subprocess.check_output("adb start-server").decode('utf-8')
             print(two)
+            print("Attempting adb connect on " + tv)
             three = subprocess.check_output("adb connect " + tv).decode('utf-8')
             print(three)
+            print("Attempting db shell settings put secure sleep_timeout 0 on " + tv)
             four = subprocess.check_output("adb shell settings put secure sleep_timeout 0").decode('utf-8')
             print(four)
+            print("Attempting adb shell settings put system screen_off_timeout 2147460000 on " + tv)
             five = subprocess.check_output("adb shell settings put system screen_off_timeout 2147460000").decode('utf-8')
             print(five)
+            print("Attempting adb disconnect on " + tv)
             six = subprocess.check_output("adb disconnect " + tv).decode('utf-8')
             print(six)
+            print("Attempting adb kill-server on " + tv)
             seven = subprocess.check_output("adb kill-server").decode('utf-8')
             print(seven)
     except:
-        print("Input has to be a number. (Or command failed)")
-#yeet
+        print("***Input has to be a number. (Or command failed)***")
+        print("***If you did input a number then the command likely failed.***")
+        print("***Can you contact the TV? Try pinging it. Are you on the proper wifi network?***")
+        print("***Remember: The Wifi network needs to be opened up. Message Justin C.***")
+        print("***If it failed right away with no delay, did you install ADB?***")
