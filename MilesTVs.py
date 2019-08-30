b4bd99df03dbb1c1202919e66b99d5617f128d8c
Cody Spering <43628079+SexyJesus@users.noreply.github.com>
2019-08-30 08:51:11 -0400

    Updated IP Array
    Removed first 2 IP octets since they're the same always and changed commands to just append last 2 octets.

--- MilesTVs.py
+++ MilesTVs.py
@@ -34,25 +34,26 @@ print("")
 while True:
     tvSelect = input(">Make a TV Selection (Ctrl-C to quit): ")
 
+    # First 2 octets are always the same so array is just last 2 of IPs
     tvArr = [
-    "172.19.16.10",
-    "172.19.16.12",
-    "172.19.16.36",
-    "172.19.16.28",
-    "172.19.16.211",
-    "172.19.16.212",
-    "172.19.16.7",
-    "172.19.16.16",
-    "172.19.16.18",
-    "172.19.16.204",
-    "172.19.16.11",
-    "172.19.16.245",
-    "172.19.16.69",
-    "172.19.16.27",
-    "172.19.23.219",
-    "172.19.20.152",
-    "172.19.16.8",
-    "172.19.23.195"
+    "16.10",
+    "16.12",
+    "16.36",
+    "16.28",
+    "16.211",
+    "16.212",
+    "16.7"
+    "16.16",
+    "16.18",
+    "16.204",
+    "16.11",
+    "16.245",
+    "16.69",
+    "16.27",
+    "23.219",
+    "20.152",
+    "16.8",
+    "23.195"
     ]
 
     try:
@@ -63,26 +64,26 @@ while True:
             print("***Please select a number from 1-18***")
         else:
             tv = tvArr[tvArrNum]
-            #ADB commands below, may need to be cleaned up, may also not even work, sue me.
-            print("Attempting adb kill-server on " + tv)
+            #ADB commands below
+            print("Attempting adb kill-server on 172.19." + tv)
             one = subprocess.check_output("adb kill-server").decode('utf-8')
             print(one)
-            print("Attempting adb start-server on " + tv)
+            print("Attempting adb start-server on 172.19." + tv)
             two = subprocess.check_output("adb start-server").decode('utf-8')
             print(two)
-            print("Attempting adb connect on " + tv)
-            three = subprocess.check_output("adb connect " + tv).decode('utf-8')
+            print("Attempting adb connect on 172.19." + tv)
+            three = subprocess.check_output("adb connect 172.19." + tv).decode('utf-8')
             print(three)
-            print("Attempting db shell settings put secure sleep_timeout 0 on " + tv)
+            print("Attempting db shell settings put secure sleep_timeout 0 on 172.19." + tv)
             four = subprocess.check_output("adb shell settings put secure sleep_timeout 0").decode('utf-8')
             print(four)
-            print("Attempting adb shell settings put system screen_off_timeout 2147460000 on " + tv)
+            print("Attempting adb shell settings put system screen_off_timeout 2147460000 on 172.19." + tv)
             five = subprocess.check_output("adb shell settings put system screen_off_timeout 2147460000").decode('utf-8')
             print(five)
-            print("Attempting adb disconnect on " + tv)
-            six = subprocess.check_output("adb disconnect " + tv).decode('utf-8')
+            print("Attempting adb disconnect on 172.19." + tv)
+            six = subprocess.check_output("adb disconnect 172.19." + tv).decode('utf-8')
             print(six)
-            print("Attempting adb kill-server on " + tv)
+            print("Attempting adb kill-server on 172.19." + tv)
             seven = subprocess.check_output("adb kill-server").decode('utf-8')
             print(seven)
     except:
